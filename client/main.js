6473f6d76eae157895b9606c37654c450edd57ba
Michael Speiser <speiserm2@mailbox.winthrop.edu>
2019-04-10 13:49:58 -0400

    Update

--- client/main.js
+++ client/main.js
@@ -4,23 +4,49 @@ import {Meteor} from 'meteor/meteor';
 import {Candidates} from './../imports/api/candidates.js';
 import {Tracker} from 'meteor/tracker';
 
-const renderCandidates = function(candidateList){
-return candidateList.map(function(candidate){
-  return <p key={candidate._id}> {candidate.name} has {candidate.votes} vote(s)</p>;
+const renderCandidates = (candidateList) => {
+return candidateList.map((candidate) => {
+  return (
+    <p key={candidate._id}>
+    {candidate.name} has {candidate.votes} vote(s){' '}
+    <button onClick={() => (
+      alert("Are you sure you want to delete that candidate?"),
+
+      Candidates.remove({_id: candidate._id})
+    )}>X</button>
+    </p>);
   });
 };
 
-Meteor.startup(function(){
-  Candidates.insert({
-    name: 'joe',
-    votes: 3,
-  });
-  Tracker.autorun(function(){
+Meteor.startup(() => {
+  const proccessFormDataFunction = (event) => {
+    event.preventDefault();
+    let candidateName = event.target.formInputNameAttribute.value;
+
+    if(candidateName){
+      Candidates.insert({
+          name: candidateName,
+          votes: 1,
+        });
+    };
+    event.target.formInputNameAttribute.value = '';
+  };
+
+
+//Candidates.insert({
+    //name: 'joe',
+    //votes: 3,
+  //});
+  Tracker.autorun(() => {
   //const candidates = Candidates.find().fetch();
   let title = "The big election";
   let jsx = (
     <div>
       <h1>{title}</h1>
+      <form onSubmit={proccessFormDataFunction}>
+        <input type="text" name="formInputNameAttribute" placeholder="candidate name" />
+        <button> Add candidate</button>
+      </form>
       {renderCandidates(Candidates.find().fetch())}
     </div>
 );
--- server/main.js
+++ server/main.js
@@ -3,10 +3,53 @@ import {Meteor} from 'meteor/meteor';
 import {Candidates} from './../imports/api/candidates.js';
 
 Meteor.startup(function(){
-  Candidates.insert({
-name: 'pat',
-votes: 5,
+  /*
+  let square = function(x) {
 
-  });
-  
+    return x*x;
+  };
+  console.log(square(5));
+
+  function square2(x) {
+    return x*x;
+  };
+  console.log(square2(3));
+
+  let square3 = (x) => {
+    return x*x;
+  };
+  console.log(square3(6));
+
+  let square4 = (x) => x*x;
+  console.log(square4(4));
+
+
+  let user = {
+    name: 'newman',
+    sayHi: function(){
+      console.log(this.name);
+    },
+  };
+  user.sayHi();
+
+  let user2 = {
+    name: 'freeman',
+    sayHi: () => {
+      console.log(user2.name);
+    },
+  };
+  user2.sayHi();
+*/
+
+let numbers=[3, 12, 54, 17];
+let newNumbers= numbers.map(function(number){
+  return number+1;
+});
+
+console.log(newNumbers);
+
+let newNumbers2= numbers.map((number) => number+1);
+
+console.log(newNumbers2);
+
 });
